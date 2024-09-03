50f524cf3f5ee039f4689a4c41df2f0fbe49fd75
Anoxia7268 <anoxia7268@gmail.com>
2024-09-03 17:32:43 +0800

    feat:"[3] 无重复字符的最长子串"

---
+++ 3.无重复字符的最长子串/3.无重复字符的最长子串.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=golang
+ *
+ * [3] 无重复字符的最长子串
+ *
+ * https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (40.15%)
+ * Likes:    10302
+ * Dislikes: 0
+ * Total Accepted:    3M
+ * Total Submissions: 7.5M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= s.length <= 5 * 10^4
+ * s 由英文字母、数字、符号和空格组成
+ *
+ *
+ */
+package main
+
+// @lc code=start
+func lengthOfLongestSubstring(s string) int {
+	dict := [128]bool{}
+	length, max := 0, 0
+	for i, j := 0, 0; i < len(s); i++ {
+		index := s[i]
+		if dict[index] {
+			for ; dict[index]; j++ {
+				length--
+				dict[s[j]] = false
+			}
+		}
+
+		dict[index] = true
+		length++
+		if length > max {
+			max = length
+		}
+	}
+	return max
+}
+
+// @lc code=end
